0f452b7c4806cce8d4ad8a2d2edcca9b1441e7ad
JKW1008 <todendvv91@gmail.com>
2023-08-27 00:41:13 +0900

    test

--- src/community/inc/member.php
+++ src/community/inc/member.php
@@ -36,19 +36,47 @@
 
         // 회원정보 입력
         public function input($marr){
+
+            // 단방향 암호화 
+            $new_hash_password = password_hash($marr['password'], PASSWORD_DEFAULT);
+
             $sql = "INSERT INTO users(id, name, password, email, zipcode, addr1, addr2, photo, create_at, ip) VALUES
                     (:id, :name, :password, :email, :zipcode, :addr1, :addr2, :photo, NOW(), :ip)";
             $stmt = $this->conn->prepare($sql);
             $stmt->bindParam(':email'   , $marr['email']);
             $stmt->bindParam(':id'      , $marr['id']);
             $stmt->bindParam(':name'    , $marr['name']);
-            $stmt->bindParam(':password', $marr['password']);
+            $stmt->bindParam(':password', $new_hash_password);
             $stmt->bindParam(':zipcode' , $marr['zipcode']);
             $stmt->bindParam(':addr1'   , $marr['addr1']);
             $stmt->bindParam(':addr2'   , $marr['addr2']);
             $stmt->bindParam(':photo'   , $marr['photo']);
             $stmt->bindParam(':ip'      , $_SERVER['REMOTE_ADDR']);
             $stmt->execute();
+        }
+
+        // 로그인
+        public function login($id, $pw){
+
+            // password_verify($password, $new_password);
+
+            $sql = "SELECT password FROM users WHERE id=:id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+
+            if($stmt->rowCount()){
+                $row = $stmt->fetch();
+
+                if(password_verify($pw, $row['password'])){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+
         }
     }
 ?>
---
+++ src/community/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $g_title = '회원가입을 축하드립니다.';
+    $js_array = ['js/home.js'];
+
+    $menu_code = 'home';
+
+    include 'inc_header.php';
+?>
+<main class="w-75 mx-auto border rounded-5 p-5 d-flex gap-5" style="height: calc(100vh - 257px);">
+    <img src="images/logo.svg" alt="" class="w-50">
+    <div>
+        <h3>HOME 입니다.</h3>
+    </div>
+</main>
+<?php
+    include 'inc_footer.php';
+?>
